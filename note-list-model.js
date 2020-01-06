edcf1ad2cc627372d72c0c974c31dd72c3e77445
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-06 17:39:47 +0000

    Add view method to see list of notes

--- note-list-model.js
+++ note-list-model.js
@@ -7,8 +7,12 @@
     newNote: function(text) {
       var note = new Note(text);
       this._list.push(note);
+    },
+
+    view: function() {
+      return this._list
     }
-  }
+  };
 
   exports.NoteList = NoteList;
 
--- note-list-tests.js
+++ note-list-tests.js
@@ -1,8 +1,19 @@
 function testCreateNote(content) {
   var noteList = new NoteList();
   noteList.newNote(content);
-  actual = noteList._list[0].read()
+  actual = noteList.view()[0].read();
   assert.toInclude(content, actual);
 }
 
 testCreateNote("Hello World!");
+
+function testViewList(content, content2) {
+  var noteList = new NoteList();
+  noteList.newNote(content);
+  noteList.newNote(content2);
+  assert.eq(2, noteList.view().length);
+  assert.eq(content, noteList.view()[0].read());
+  assert.eq(content2, noteList.view()[1].read());
+}
+
+testViewList("Hello World!", "2nd test");
